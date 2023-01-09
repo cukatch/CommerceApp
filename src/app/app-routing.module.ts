28a4b4064b0efdb0d069ab16bf80436698dfe06f
kech <jeice410@gmail.com>
2023-01-09 11:18:36 -0500

    skeleton done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,16 @@
 import { Routes } from '@angular/router';
-import { CartComponent } from './cart/cart/cart.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductCardComponent } from './product/product-card/product-card.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { CartComponent } from './cart/components/cart.component';
+
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'products', component:
-      ProductListComponent
-  },
-  {
-    path: 'checkout', component:
-      CartComponent
-  },
-  {
-    path: '', redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: '**', component:
-      ErrorPageComponent
-  }];
-
-export class AppRoutingModule { }
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductCardComponent},
+  {path: 'checkout', component: CartComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: ErrorPageComponent}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
